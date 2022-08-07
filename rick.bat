1c497c1a82e5141eeb32a0a51756895cf3a33975
JayPlay71 <63882628+JayPlay71@users.noreply.github.com>
2022-08-06 22:25:24 -0700

    Add files via upload

---
+++ rick.bat
@@ -0,0 +1,251 @@
+@echo off
+
+:x
+
+start https://www.youtube.com/watch?v=xm3YgoEiEDc
+
+timeout /t 1
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("We're no strangers to love", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert.vbs
+
+wscript %temp%\alert.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("You know the rules and so do I (do I)", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert1.vbs
+
+wscript %temp%\alert1.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("A full commitment's what I'm thinking of", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert2.vbs
+
+wscript %temp%\alert2.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("You wouldn't get this from any other guy", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert3.vbs
+
+wscript %temp%\alert3.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("I just wanna tell you how I'm feeling", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert4.vbs
+
+wscript %temp%\alert4.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Gotta make you understand", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert5.vbs
+
+wscript %temp%\alert5.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna give you up", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert6.vbs
+
+wscript %temp%\alert6.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna let you down", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert7.vbs
+
+wscript %temp%\alert7.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna run around and desert you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert8.vbs
+
+wscript %temp%\alert8.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna make you cry", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert9.vbs
+
+wscript %temp%\alert9.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna say goodbye", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert10.vbs
+
+wscript %temp%\alert10.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna tell a lie and hurt you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert11.vbs
+
+wscript %temp%\alert11.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("We've known each other for so long", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert12.vbs
+
+wscript %temp%\alert12.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Your heart's been aching, but you're too shy to say it (say it)", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert13.vbs
+
+wscript %temp%\alert13.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Inside, we both know what's been going on (going on)", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert14.vbs
+
+wscript %temp%\alert14.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("We know the game and we're gonna play it", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert15.vbs
+
+wscript %temp%\alert15.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("And if you ask me how I'm feeling", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert16.vbs
+
+wscript %temp%\alert16.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Don't tell me you're too blind to see", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert17.vbs
+
+wscript %temp%\alert17.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna give you up", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert18.vbs
+
+wscript %temp%\alert18.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna let you down", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert19.vbs
+
+wscript %temp%\alert19.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna run around and desert you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert20.vbs
+
+wscript %temp%\alert20.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna make you cry", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert21.vbs
+
+wscript %temp%\alert21.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna say goodbye", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert22.vbs
+
+wscript %temp%\alert22.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna tell a lie and hurt you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert23.vbs
+
+wscript %temp%\alert23.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna give you up", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert24.vbs
+
+wscript %temp%\alert24.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna let you down", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert25.vbs
+
+wscript %temp%\alert25.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna run around and desert you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert26.vbs
+
+wscript %temp%\alert26.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna make you cry", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert27.vbs
+
+wscript %temp%\alert27.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna say goodbye", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert28.vbs
+
+wscript %temp%\alert28.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna tell a lie and hurt you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert29.vbs
+
+wscript %temp%\alert29.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("We've known each other for so long", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert30.vbs
+
+wscript %temp%\alert30.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Your heart's been aching, but you're too shy to say it (say it)", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert31.vbs
+
+wscript %temp%\alert31.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Inside, we both know what's been going on (going on)", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert32.vbs
+
+wscript %temp%\alert32.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("We know the game and we're gonna play it", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert33.vbs
+
+wscript %temp%\alert33.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("I just wanna tell you how I'm feeling", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert34.vbs
+
+wscript %temp%\alert34.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Gotta make you understand", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert35.vbs
+
+wscript %temp%\alert35.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna give you up", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert36.vbs
+
+wscript %temp%\alert36.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna let you down", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert37.vbs
+
+wscript %temp%\alert37.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna run around and desert you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert38.vbs
+
+wscript %temp%\alert38.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna make you cry", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert39.vbs
+
+wscript %temp%\alert39.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna say goodbye", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert40.vbs
+
+wscript %temp%\alert40.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna tell a lie and hurt you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert41.vbs
+
+wscript %temp%\alert41.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna give you up", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert42.vbs
+
+wscript %temp%\alert42.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna let you down", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert43.vbs
+
+wscript %temp%\alert43.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna run around and desert you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert44.vbs
+
+wscript %temp%\alert44.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna make you cry", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert45.vbs
+
+wscript %temp%\alert45.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna say goodbye", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert46.vbs
+
+wscript %temp%\alert46.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna tell a lie and hurt you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert47.vbs
+
+wscript %temp%\alert47.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna give you up", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert48.vbs
+
+wscript %temp%\alert48.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna let you down", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert49.vbs
+
+wscript %temp%\alert49.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna run around and desert you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert50.vbs
+
+wscript %temp%\alert50.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna make you cry", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert51.vbs
+
+wscript %temp%\alert51.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna say goodbye", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert52.vbs
+
+wscript %temp%\alert52.vbs
+
+@echo X=WScript.CreateObject("WScript.Shell").Popup("Never gonna tell a lie and hurt you", 20, "Virus!", 0 + 48 + 4096) >%temp%\alert53.vbs
+
+wscript %temp%\alert53.vbs
+
+timeout /t 1
+
+@echo off
+Call :YesNoBox "Do you want to exit?"
+if "%YesNo%"=="6" (
+GOTO x
+exit /b
+)
+
+exit /b
+:YesNoBox
+REM returns 6 = Yes, 7 = No. Type=4 = Yes/No
+set YesNo=
+set MsgType=4
+set heading=%~2
+set message=%~1
+echo wscript.echo msgbox(WScript.Arguments(0),%MsgType% Or vbSystemModal,WScript.Arguments(1)) >"%temp%\input.vbs"
+for /f "tokens=* delims=" %%a in ('cscript //nologo "%temp%\input.vbs" "%message%" "%heading%"') do set YesNo=%%a
+exit /b
+
+:MessageBox
+set heading=%~2
+set message=%~1
+echo msgbox WScript.Arguments(0),0,WScript.Arguments(1) >"%temp%\input.vbs"
+cscript //nologo "%temp%\input.vbs" "%message%" "%heading%"
+exit /b
+
